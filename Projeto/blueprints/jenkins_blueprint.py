4f5483a4acfca4c7a6ad3c1031b0428180ccdc42
iromato <iromato@hotmail.com>
2020-03-12 21:44:42 -0300

    aula07

--- Projeto/blueprints/jenkins_blueprint.py
+++ Projeto/blueprints/jenkins_blueprint.py
@@ -8,6 +8,32 @@ def index():
     client = jenkins.Jenkins('http://localhost:8080', username='admin',password='admin')
     jobs_list = client.get_jobs()
     jobs = []
+
     for job in jobs_list:
         jobs.append(client.get_job_info(job['fullname']))
     return flask.render_template('jenkins.html',titulo='Jobs Jenkins',jobs=jobs)
+
+@jenkins_routes.route('/build/<string:job_name>')
+def jenkins_build(job_name):
+    client = jenkins.Jenkins('http://localhost:8080',username='admin',password='admin')
+    client.build_job(job_name)
+    return flask.redirect(flask.url_for('jenkins.index'))
+
+@jenkins_routes.route('/update/<string:job_name>')
+def jenkins_update(job_name):
+    client = jenkins.Jenkins('http://localhost:8080',username='admin',password='admin')
+    job = {
+        'xml' : client.get_job_config(job_name)
+    }
+    print(client.get_job_config(job_name))
+    return flask.render_template('jenkins_update.html',job=job,titulo=job_name,job_name=job_name)
+
+@jenkins_routes.route('/rebuild',methods=['POST'])
+def jenkins_rebuild():
+    try:
+        data = flask.request.form
+        client = jenkins.Jenkins('http://localhost:8080',username='admin',password='admin')
+        client.reconfig_job(data['name'],data['xml'])
+        return flask.redirect(flask.url_for('jenkins.index'))
+    except Exception:
+        return flask.redirect(flask.url_for('jenkins.jenkins_update',job_name=data['name']))
